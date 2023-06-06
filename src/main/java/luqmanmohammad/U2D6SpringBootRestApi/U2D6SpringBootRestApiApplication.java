bff9bc19fa3790d5100c80a3d482ff73f46a2e20
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-06 15:01:20 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/U2D6SpringBootRestApi/U2D6SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D6SpringBootRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D6SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D6SpringBootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/U2D6SpringBootRestApi/U2D6SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D6SpringBootRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D6SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
